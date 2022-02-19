3c1aeb52331638077f5236f714f57647ccb25858
gladius <gladiusservices@protonmail.com>
2022-02-19 16:18:04 +0100

    Update logic to read map files from saves path: only actual map files

---
+++ src/core/services/getMapFiles.service.ts
@@ -0,0 +1,13 @@
+import paths from '../../config/paths';
+import { readdir } from 'fs/promises';
+
+const getMapFiles = async () => {
+  const savesFiles = await readdir(paths.SAVES_DATA_FOLDER_PATH);
+  const mapFiles = savesFiles.filter(file => file.startsWith('map') && file.endsWith('.bin'));
+  if (!mapFiles.length) {
+    throw new Error('Missing saves to clean up');
+  }
+  return mapFiles;
+};
+
+export default getMapFiles;
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,19 @@
 import getCoordsToPurgeFrom from './core/services/getCoordsToPurgeFrom.service';
 import getFileContent from './helpers/getFileContent.helper';
+import getMapFiles from './core/services/getMapFiles.service';
 import paths from './config/paths';
-import { readdir } from 'fs/promises';
 
 const main = async () => {
   console.info('[ ZOMBOB ] Map cleaner started');
 
-  const mapFiles = await readdir(paths.SAVES_DATA_FOLDER_PATH);
-  if (!mapFiles.length) {
-    throw new Error('Missing saves to clean up');
-  }
+  const mapFiles = getMapFiles();
   console.info('[ ZOMBOB ] Map files');
   console.table(mapFiles);
 
   const rawCoordsToPurge = await getFileContent(paths.COORDS_TO_PURGE_FILE_PATH);
-
   const coordsToPurge = rawCoordsToPurge.map(rawCoords => getCoordsToPurgeFrom(rawCoords));
   console.info('[ ZOMBOB ] Coords to purge');
   console.table(coordsToPurge);
-
 };
 
 main().then(console.info).catch(console.warn);
