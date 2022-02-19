7caae86794523b7a5d65eeb9d4d05d89494e54d9
gladius <gladiusservices@protonmail.com>
2022-02-19 15:49:33 +0100

    Update paths configuration to declare saves data folder path

--- src/config/paths.ts
+++ src/config/paths.ts
@@ -1,14 +1,17 @@
+import getEnvValueBy from '../helpers/getEnvValueBy.helper';
 import { homedir } from 'os';
 import path from 'path';
 
 const ZOMBOB_DATA_FOLDER_PATH = path.join( homedir(), 'zombob-data' );
 const MAP_CLEANER_FOLDER_PATH = path.join( ZOMBOB_DATA_FOLDER_PATH, 'map-cleaner' );
 const COORDS_TO_PURGE_FILE_PATH = path.join( MAP_CLEANER_FOLDER_PATH, 'coords_to_purge.txt' );
+const SAVES_DATA_FOLDER_PATH = path.join( homedir(), 'Zomboid', 'Saves', 'Multiplayer', getEnvValueBy('SERVER_NAME') );
 
 const paths = {
   ZOMBOB_DATA_FOLDER_PATH,
   MAP_CLEANER_FOLDER_PATH,
   COORDS_TO_PURGE_FILE_PATH,
+  SAVES_DATA_FOLDER_PATH,
 };
 
 export default paths;
