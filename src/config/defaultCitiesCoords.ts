8fdc2952f73568f1864232fde1431e9c80a3b76d
gladius <gladiusservices@protonmail.com>
2022-02-23 22:26:51 +0100

    Create defaultCitiesCoords config to encapsulate default purgable coordinates

---
+++ src/config/defaultCitiesCoords.ts
@@ -0,0 +1,45 @@
+const vanilla = {
+  westPoint: {
+    start: '10764_6533',
+    end: '12358_7238',
+  },
+  riverside: {
+    start: '5631_5164',
+    end: '6925_5678',
+  },
+  rosewood: {
+    start: '7914_11214',
+    end: '8491_11979',
+  },
+  muldraugh: {
+    start: '10218_9107',
+    end: '11163_10790',
+  },
+  louisville: {
+    start: '11959_1051',
+    end: '14931_4256',
+  },
+  ekron: {
+    start: '7046_8150',
+    end: '7535_8596',
+  },
+  marchridge: {
+    start: '9719_12517',
+    end: '10551_13225',
+  },
+};
+const modded = {
+  pistop: {
+    start: '897_1107',
+    end: '993_1171',
+  },
+  newEkron: {
+    start: '685_801',
+    end: '756_870',
+  },
+};
+
+export const defaultCitiesCoords = {
+  vanilla,
+  modded,
+};
